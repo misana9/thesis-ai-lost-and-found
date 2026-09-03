b9918ad68d0efadf6ff22dc5c29caf8461ea9b49
misana9 <once.jd2@gmail.com>
2026-09-03 09:19:49 +0800

    fix env

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,9 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings,  SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=None)
+
     database_hostname: str
     database_port: int
     database_name: str
